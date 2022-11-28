098e5aea3c29994feef2d806e7450660d7651e2a
Paul Annesley <paul@annesley.cc>
2022-11-28 20:55:04 +1030

    bundle gem safer_redis

---
+++ lib/safer_redis.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "safer_redis/version"
+
+module SaferRedis
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/safer_redis/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SaferRedis
+  VERSION = "0.1.0"
+end
